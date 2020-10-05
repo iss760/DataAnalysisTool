2db4ceba8d98802c3c3dd77f8978f62a439c0ecc
iss760 <gane774@naver.com>
2020-10-05 16:26:14 +0900

    Add word_cloud.py

---
+++ word_cloud.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+class MyWordCloud:
+    def __init__(self):
+        self.FILE_PATH = ''
+        self.FILE_NAME = ''
+        self.FONT_NAME = "./NanumFontSetup_TTF_ALL/NanumSquareB.ttf"
+
+    def draw_word_cloud(self, text, width=1500, height=1000, background_color='white'):
+        word_cloud = WordCloud(font_path=self.FONT_NAME,
+                               max_font_size='100', min_font_size='10',
+                               width=width, height=height,
+                               background_color=background_color)
+        word_cloud = word_cloud.generate_from_text(text)
+        plt.imshow(word_cloud)
+        plt.axis('off')
+        plt.show()
+
+
+data = "dog dog cat cat dog cat dog cat"
+mwc = MyWordCloud()
+mwc.draw_word_cloud(data)
