e51e27be83222e4b180a6de2e559f0a6a56decf9
iss760 <gane774@naver.com>
2020-10-14 13:48:26 +0900

    Add filtering.py

---
+++ filtering.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+
+
+class Filtering:
+    def __init__(self):
+        self.FILE_PATH = ""
+        self.FILE_NAME = ""
+        self.FILTERING_FILE_PATH = ""
+        self.FILTERING_FILE_NAME = ""
+        self.FILTERING_KEYWORD = []
+
+    def __add__(self, other):
+
+    def make_filtering_keyword(self, path, name):
+        self.FILE_PATH = path
+        self.FILE_NAME = name
+
+    def filtering(self, data, col_name):
+        data = data[~data[col_name].str.contains('|'.join(self.FILTERING_KEYWORD), na=False)]
+
+    def filtering_
