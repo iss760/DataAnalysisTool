f101f9e9353a90cfe4b362b207dc68d51cdae6db
iss760 <gane774@naver.com>
2020-11-06 14:57:24 +0900

    Update word cloud input values

--- word_cloud.py
+++ word_cloud.py
@@ -9,28 +9,40 @@ class MyWordCloud:
         self.FILE_NAME = ''
         self.FONT_NAME = "./NanumSquareB.ttf"
 
-    def _draw_word_cloud(self, text, width=1600, height=800, background_color='white'):
+    def _draw_word_cloud(self, word2cnt, width=1200, height=800, background_color='white'):
         word_cloud = WordCloud(font_path=self.FONT_NAME,
-                               max_font_size=100, min_font_size=10,
                                width=width, height=height,
                                background_color=background_color)
-        word_cloud = word_cloud.generate_from_text(text)
+        word_cloud = word_cloud.generate_from_frequencies(word2cnt)
+        plt.figure(figsize=(12, 8))
         plt.imshow(word_cloud)
         plt.axis('off')
         plt.show()
 
-    def draw_word_cloud(self, data, width=1600, height=800, background_color='white'):
+    def count_word(self, data):
+        word2cnt = {}
+        for word in data:
+            if word not in word2cnt.keys():
+                word2cnt[word] = 1
+            else:
+                word2cnt[word] += 1
+        return word2cnt
+
+    def draw_word_cloud(self, data, width=1200, height=800, background_color='white',
+                        is_font_size_norm=False, word_color="standard"):
         if type(data[0]) == list:
-            data = [b for a in data for b in a]
-            data = ' '.join(data)
+            temp = []
+            for words in data:
+                temp += words
+            word2cnt = self.count_word(data)
         elif type(data) == list:
-            data = ' '.join(data)
-        elif type(data) == str:
-            data = data
+            word2cnt = self.count_word(data)
+        elif type(data) == dict:
+            word2cnt = data
         else:
             print('Input type error (Input type : list[[str]] or list[] or str')
 
-        self._draw_word_cloud(data, width=width, height=height, background_color=background_color)
+        self._draw_word_cloud(word2cnt, width=width, height=height, background_color=background_color)
 
 
 if __name__ == '__main__':
