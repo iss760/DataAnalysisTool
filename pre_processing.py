16196bbe78b2fbd7b52d37fe517f4121d5b64f90
iss760 <gane774@naver.com>
2021-04-02 13:06:18 +0900

    Add outlier removal function

--- pre_processing.py
+++ pre_processing.py
@@ -1,5 +1,24 @@
+import numpy as np
 import pandas as pd
-import json
+
+
+class NumericalTypeProcessing:
+    def get_outlier(self, data, col_name, weight=1.5):
+        q1 = np.percentile(data[col_name].values, 25)
+        q3 = np.percentile(data[col_name].values, 75)
+        iqr = q3 - q1
+
+        iqr_weight = iqr * weight
+        lowest = q1 - iqr_weight
+        highest = q3 + iqr_weight
+
+        outlier_idx = data[col_name][(data[col_name] < lowest) | (data[col_name] > highest)].index
+        return outlier_idx
+
+    def remove_outlier_use_iqr(self, data, col_name, weight=1.5):
+        outlier_idx = self.get_outlier(data, col_name=col_name, weight=weight)    # 이상치 탐색
+        data.drop(outlier_idx, axis=0, inplace=True)    # 이상치 제거
+        return data
 
 
 class Filtering:
