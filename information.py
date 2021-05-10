32898607417e5cbc4a9ab6a6135e2d99abf9a799
iss760 <gane774@naver.com>
2021-05-10 15:20:08 +0900

    Add print box plot in print_statistics_ind function

--- information.py
+++ information.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# 기본 정보 출력 클래스
 class Information:
     @staticmethod
     # 데이터의 기본정보를 출력하는 함수
@@ -13,9 +14,7 @@ class Information:
         """
         print("Data shape : ", data.shape, end='\n\n')
         print(data.info(), end='\n\n')
-        print("Data Sample (head) \n", data.head(), end='\n\n')
-        print("Data Sample (tail) \n", data.tail(), end='\n\n')
-        print("Data Null Sum \n", data.isnull().sum(), end='\n\n')
+        print("Data Null Sum Percent \n", round(data.isnull().sum()/data.shape[0] * 100, 2), end='\n\n')
 
     @staticmethod
     # 수치형 데이터의 평균, 중위값 등 기본적 통계 지표를 출력하는 함수
@@ -25,11 +24,15 @@ class Information:
         :param col_name: (str) Column name for which you want to see indicators
         :return: None
         """
+        print("Data Max : ", data[col_name].max())
+        print("Data Min : ", data[col_name].min())
         print("Data Mean : ", data[col_name].mean())
         print("Data Median : ", data[col_name].median())
         print("Data Variance : {0: .3f}".format(data[col_name].var()))
         print("Data Standard deviation: {0: .3f}".format(data[col_name].std()))
         print()
+        plt.boxplot(data[col_name])
+        plt.show()
 
 
 class Visualization:
