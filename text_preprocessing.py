76b05a35346be9a9add068c29a0ec98bae6195dc
iss760 <gane774@naver.com>
2022-03-14 09:58:40 +0900

    Modify validation name

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -60,18 +60,18 @@ class TextProcessing(Utility):
             r'[^A-Za-z0-9.,\'!?#\u0020\n\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF'
             r'\uFE70-\uFEFF\u10E6-\u10E7\u1EC7-\u1ECB\u1EE0-\u1EEF]')
 
-        # 언어별 유니코드 범위
+        # HTML 모음
+        self.HTML_TAGS = ['<em>', '</em>', '<br>', '</br>']
+
+        # 자주 쓰이는 밈 표현
+        self.MEME = [r'ㅋ+', r'ㅎ+', r'ㅜ+', r'\^\^', r':\)', r'~+', r'!+']
+
+        # 언어별 유니코드 범위 (언어: re.compile(유니코드 범위))
         # keys: latin, greek, russian, korean_whole, korean, chinese, japanese, arabic
         self.REX_LANGUAGE2UNICODE = self.load_util_json('rex_language2unicode.json')
         self.REX_LANGUAGE2UNICODE = dict(map(lambda x: (x[0], re.compile(x[1])), self.REX_LANGUAGE2UNICODE.items()))
 
-        # HTML 모음
-        self.html_tags = ['<em>', '</em>', '<e m>', '<br>', '</br>']
-
-        # 자주 쓰이는 밈 표현
-        self.meme = [r'ㅋ+', r'ㅎ+', r'ㅜ+', r'\^\^', r':\)', r'~+', r'!+']
-
-        # 기호별 유니코드
+        # 기호별 유니코드 (특수기호: re.compile(유니코드들))
         # keys : comma, colon, dash, exclamation, bullet, hyphen, double_hyphen, question_mark, single_quotation_mark,
         #        corner_bracket_left, corner_bracket_right, round_bracket_left, angle_bracket_left, angle_bracket_right,
         #        box_bracket_left, box_bracket_right, full_stop, white_space, ascii_symbol
@@ -79,25 +79,24 @@ class TextProcessing(Utility):
         self.REX_SYMBOL2UNICODE = dict(map(lambda x: (x[0], re.compile(x[1])), self.REX_SYMBOL2UNICODE.items()))
 
         # 전각 반자 유니코드 (전각: 반자)
-        self.unicode_full2half = self.load_util_json('unicode_full2half.json')
+        self.UNICODE_FULL2HALF = self.load_util_json('unicode_full2half.json')
 
         # 자주 쓰이는 오타와 정답 (오타: 정답)
-        self.grammar_typo2cor = self.load_util_json('grammar_typo2cor.json')
+        self.GRAMMAR_TYPO2COR = self.load_util_json('grammar_typo2cor.json')
 
         # 자주 쓰이는 영어표현의 한국어 표현 (영어: 한국어)
-        self.grammar_en2kr = self.load_util_json('grammar_en2kr.json')
+        self.GRAMMAR_EN2KR = self.load_util_json('grammar_en2kr.json')
 
         # 키위 형태소 분석기의 형태소 영어명과 한국명 (형태소 영어명: 형태소 한국명)
-        self.kiwi_tag2pos = self.load_util_json('kiwi_tag2pos.json')    # 긍정지시사: ~이다 / 부정지시가: ~아니다
+        self.KIWI_TAG2POS = self.load_util_json('kiwi_tag2pos.json')    # 긍정지시사: ~이다 / 부정지시가: ~아니다
 
     # 특수기호 유니코드 정규화 메서드
     def normalize_special_symbol(self, doc):
         pass
 
-
     # 영어로된 kiwi 태그를 한국어 형태소로 변환하는 함수
     def convert_tag2pos(self, s):
-        return self.kiwi_tag2pos[s]
+        return self.KIWI_TAG2POS[s]
 
     # FullWidth(전자) 문자를 HalfWidth(반자) 문자로 변환해주는 메서드
     def convert_full_to_half(self, df, col):
@@ -106,7 +105,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        return df.replace({col: self.unicode_full2half})
+        return df.replace({col: self.UNICODE_FULL2HALF})
 
     # HTML Tag 제거 메서드
     def remove_html_tags(self, df, col):
@@ -115,7 +114,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        for tag in self.html_tags:
+        for tag in self.HTML_TAGS:
             df[col] = df[col].str.replace(tag, " ")
 
         return df
@@ -127,7 +126,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        for tag in self.html_tags:
+        for tag in self.HTML_TAGS:
             df[col] = df[col].str.replace(tag, " ")
 
         return df
@@ -139,7 +138,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        for k, v in self.grammar_typo2cor.items():
+        for k, v in self.GRAMMAR_TYPO2COR.items():
             df[col] = df[col].str.replace(k, v)
 
         return df
@@ -151,7 +150,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        for en, kr in self.grammar_en2kr.items():
+        for en, kr in self.GRAMMAR_EN2KR.items():
             df[col].replace(to_replace=en, value=kr, regex=True, inplace=True)
 
         return df
@@ -164,7 +163,7 @@ class TextProcessing(Utility):
         :param col: (str or list) column name or columns name
         :return: converted data to half width from full width
         """
-        for v in self.meme:
+        for v in self.MEME:
             df[col].replace(to_replace='', value=v, regex=True, inplace=True)
 
         return df
